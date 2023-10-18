b2ff5e4490daf05368330d872ce2f8c22154ee02
thicco-catto <cristobal.martin01@gmail.com>
2023-10-18 18:25:15 +0200

    fix: Fixed form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,17 @@
 import { Form } from "react-bootstrap";
 
-export default function login() {
+export default function Login() {
     return <>
-    <Form>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Example textarea</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-      </Form.Group>
-    </Form>
+        <Form>
+            <div>
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com"/>
+            </div>
+
+            <div>
+                <Form.Label>Example textarea</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+            </div>
+        </Form>
     </>;
 }
