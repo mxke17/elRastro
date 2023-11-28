81a32a2adfcc76afdfba6f0571974cdb668e7233
mxke17 <80281934+mxke17@users.noreply.github.com>
2023-11-28 17:13:34 +0100

    fix: update some home page photos

--- app/page.tsx
+++ app/page.tsx
@@ -9,30 +9,30 @@ import Figure from "react-bootstrap/Figure";
 export default async function Home() {
   return (
     <>
-    <NavbarLite></NavbarLite>
-    <Carousel>
-      <Carousel.Item>
-        <Figure.Image src="/coche.jpg" style={{ objectFit: "cover", objectPosition: "center", width: "100%", height: "700px" }}/>
-        <Carousel.Caption>
-          <h3>Automóviles</h3>
-          <p>Vendo mi coche porque mi sueño de ser piloto de carreras fue solo un sueño, y ahora necesito espacio para mis otras fantasías</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Figure.Image src="/pc.jpg" style={{ objectFit: "cover", objectPosition: "center", width: "100%", height: "700px" }}/>
-        <Carousel.Caption>
-          <h3>Ordenadores</h3>
-          <p>Se vende ordenador de segunda mano: ha visto más memes de los que puedes imaginar, pero todavía le queda mucha risa en su disco duro.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Figure.Image src="/movil.jpg" style={{ objectFit: "cover", objectPosition: "center", width: "100%", height: "700px" }}/>
-        <Carousel.Caption>
-          <h3>Smartphones</h3>
-          <p>Este móvil es como un superhéroe retirado: ya no es el más nuevo, pero sigue siendo increíblemente útil cuando lo necesitas. ¡Dale una nueva misión!</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-    <FooterHome></FooterHome></>
+      <NavbarLite></NavbarLite>
+      <Carousel>
+        <Carousel.Item>
+          <Figure.Image src="/pc.jpg" style={{ objectFit: "cover", objectPosition: "center", width: "100%", height: "700px" }} />
+          <Carousel.Caption>
+            <h3>Ordenadores</h3>
+            <p>Se vende ordenador de segunda mano: ha visto más memes de los que puedes imaginar, pero todavía le queda mucha risa en su disco duro.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Figure.Image src="/coche.jpg" style={{ objectFit: "cover", objectPosition: "center", width: "100%", height: "700px" }} />
+          <Carousel.Caption>
+            <h3>Automóviles</h3>
+            <p>Vendo mi coche porque mi sueño de ser piloto de carreras fue solo un sueño, y ahora necesito espacio para mis otras fantasías</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Figure.Image src="/movil.jpg" style={{ objectFit: "cover", objectPosition: "center", width: "100%", height: "700px" }} />
+          <Carousel.Caption>
+            <h3>Smartphones</h3>
+            <p>Este móvil es como un superhéroe retirado: ya no es el más nuevo, pero sigue siendo increíblemente útil cuando lo necesitas. ¡Dale una nueva misión!</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <FooterHome></FooterHome></>
   );
 }
