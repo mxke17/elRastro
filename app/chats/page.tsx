215d7940db1b302b8430fbe3a5198fb6eaa59d0b
NikitaKulyukin <nikitak@uma.es>
2023-11-30 00:45:36 +0100

    feat: Modified Chat to work with database

--- app/chats/page.tsx
+++ app/chats/page.tsx
@@ -1,12 +1,18 @@
 import { ChatList } from "@/components/chatList";
 import { FooterHome } from "@/components/footer";
 import { NavbarHome } from "@/components/navbar";
-import { Chat } from "@/database/chats";
+import { GetAllChatsOfUser } from "@/database/chats";
 
-export default function chats(){
+export default async function chats(){
+    const userId = "653be37c5ee549bea86cd462";
+    const allChats = await GetAllChatsOfUser(userId);
+
+    if(allChats===null){
+        return <h1>ERROR</h1>;
+   }
     return <>
         <NavbarHome></NavbarHome>
-        <ChatList chatList={[new Chat, new Chat]}></ChatList>
+        <ChatList chatList={allChats}></ChatList>
         <FooterHome></FooterHome>
     </>;
 }
