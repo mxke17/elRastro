942deb772af4fea631397826c281b707340f8657
Carlos-DE <carlosdominguezenriquez611@gmail.com>
2023-12-01 18:38:56 +0100

    Auction Terminado

--- app/auction/[id]/page.tsx
+++ app/auction/[id]/page.tsx
@@ -1,20 +1,28 @@
 import { AuctionDetailed, AuctionMini } from "@/components/auction";
+import { BidMasAlta } from "@/components/bid";
 import { FooterHome } from "@/components/footer";
 import { NavbarHome } from "@/components/navbar";
 import { Auction, GetAuction } from "@/database/auctions";
+import { GetHighestBidForAuction } from "@/database/bid";
 
 export default async function auction(){
-    const auctionDetallada = await GetAuction("653be2fb5ee549bea86cd45a");
+    const auctionDetallada = await GetAuction("653be2fb5ee549bea86cd459");
 
     if(!auctionDetallada){
         return <>SUBASTA NO ENCONTRADA</>;
     }
 
+    const bidMaxima = await GetHighestBidForAuction(auctionDetallada.ID.toHexString());
+
+    if(!bidMaxima){
+        return <>PUJA NO ENCONTRADA</>;
+    }
+
     return <>
         <NavbarHome></NavbarHome>
         <AuctionDetailed auction={auctionDetallada}></AuctionDetailed>
+        <BidMasAlta bid={bidMaxima}></BidMasAlta>
         
-        PUJA MÁS ALTA: AQUI VA LA PUJA MÁS ALTA Y POR TANTO EL QUE SE LA VA A LLEVAR ESPABILA Y COMPRALO ANTES TÚ
         <FooterHome></FooterHome>
     </>;
 }
