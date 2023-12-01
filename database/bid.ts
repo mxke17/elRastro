bac74e32f1b1068cb5c8bae74b1db92a62bad2c9
thicco-catto <cristobal.martin01@gmail.com>
2023-12-01 17:52:34 +0100

    feat: Added function to get highest bid for auction

---
+++ database/bid.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ObjectId } from "mongodb";
+import { Get } from "./fetch";
+
+export class Bid {
+    ID: ObjectId;
+    Date: Date;
+    Quantity: number;
+    Bidder: ObjectId;
+    Auction: ObjectId;
+
+    constructor(
+        id: string,
+        date: Date,
+        quantity: number,
+        bidder: string,
+        auction: string
+    ) {
+        this.ID = ObjectId.createFromHexString(id);
+        this.Date = new Date(date);
+        this.Quantity = quantity;
+        this.Bidder = ObjectId.createFromHexString(bidder);
+        this.Auction = ObjectId.createFromHexString(auction);
+    }
+
+    static FromJSON(json: any) {
+        return new Bid(
+            json["_id"],
+            json["Fecha de puja"],
+            json["Cantidad"],
+            json["Postor"],
+            json["Subasta"],
+        );
+    }
+}
+
+export async function GetHighestBidForAuction(auctionID: string) {
+    const bid = await Get(`subastas/${auctionID}/pujaMasAlta`);
+    return Bid.FromJSON(bid);
+}
