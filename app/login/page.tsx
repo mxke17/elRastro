00c9922107c4ca4c9bb934d73b31a402c2a48a57
mxke17 <80281934+mxke17@users.noreply.github.com>
2023-12-03 09:58:43 +0100

    fix: update login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,26 +1,47 @@
-import { Button, Form } from "react-bootstrap";
+"use client";
+import { FooterHome } from "@/components/footer";
+import { NavbarLite } from "@/components/navbarLite";
+import { Button, Card, Col, Container, Figure, Form, Row } from "react-bootstrap";
 
 export default function Login() {
     return <>
-        <Form>
-            <div className="mb-3">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                    We&apos;ll never share your email with anyone else.
-                </Form.Text>
-            </div>
+        <NavbarLite></NavbarLite>
+        <Container fluid="md">
+            <Row>
+                <Col xs={1}></Col>
+                <Col>
+                    <Card style={{ marginBottom: "20px" }}>
+                        <Figure.Image
+                            style={{ objectFit: "cover", objectPosition: "center", width: "100%", height: "75px" }}
+                            src="/login.jpg"
+                        />
+                        <Card.Body>¿Listo para desatar tu poder de compra? ¡Ingresa tu contraseña y prepárate para un viaje de gangas que cambiará tu vida!</Card.Body>
+                    </Card>
 
-            <div className="mb-3">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </div>
-            <div className="mb-3">
-                <Form.Check type="checkbox" label="Check me out" />
-            </div>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
+                    <Form style={{ marginBottom: "20px" }}>
+                        <div className="mb-3">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email"/>
+                            <Form.Text className="text-muted">
+                                Tu correo está más seguro con nosotros que las recetas secretas de la abuela. ¡Ni el gato las sabe!
+                            </Form.Text>
+                        </div>
+
+                        <div className="mb-3">
+                            <Form.Label>Contraseña</Form.Label>
+                            <Form.Control type="password"/>
+                        </div>
+                        <div className="mb-3">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </div>
+                        <Button variant="primary" type="submit">
+                            Iniciar sesión
+                        </Button>
+                    </Form>
+                </Col>
+                <Col xs={1}></Col>
+            </Row>
+        </Container>
+        <FooterHome></FooterHome>
     </>;
 }
