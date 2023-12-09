905414c6b37e1f90891bc840cd6a04ee96d60b58
Carlos-DE <carlosdominguezenriquez611@gmail.com>
2023-12-09 14:36:58 +0100

    leaflet

---
+++ database/map.ts
@@ -0,0 +1,68 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ObjectId } from "mongodb";
+import { Get } from "./fetch";
+
+const PATH = "pujas";
+
+export interface BidJSON {
+    _id: string,
+    Fecha: string,
+    Cantidad: number,
+    Postor: string,
+    Subasta: string,
+}
+
+export class Bid {
+    ID: ObjectId;
+    Date: Date;
+    Quantity: number;
+    Bidder: ObjectId;
+    Auction: ObjectId;
+
+    constructor(
+        id: string,
+        date: Date,
+        quantity: number,
+        bidder: string,
+        auction: string
+    ) {
+        this.ID = ObjectId.createFromHexString(id);
+        this.Date = new Date(date);
+        this.Quantity = quantity;
+        this.Bidder = ObjectId.createFromHexString(bidder);
+        this.Auction = ObjectId.createFromHexString(auction);
+    }
+
+    static FromJSON(json: any) {
+        return new Bid(
+            json["_id"],
+            json["Fecha de puja"],
+            json["Cantidad"],
+            json["Postor"],
+            json["Subasta"],
+        );
+    }
+    ToJSON():BidJSON{
+        return {
+            _id: this.ID.toHexString(),
+            Fecha: this.Date.toISOString(),
+            Cantidad: this.Quantity,
+            Postor: this.Bidder.toHexString(),
+            Subasta: this.Auction.toHexString(),
+        };
+    }
+}
+
+export async function GetHighestBidForAuction(auctionID: string) {
+    const response = await Get(`subastas/${auctionID}/pujaMasAlta`);
+
+    try {
+        const json = await response.json();
+
+        if (!json["_id"]) { return null; }
+
+        return Bid.FromJSON(json);
+    } catch (_) {
+        return null;
+    }
+}
