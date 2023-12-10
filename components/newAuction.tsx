c5d4129e3baf6791ea2b9eb36709faba71133aa8
thicco-catto <cristobal.martin01@gmail.com>
2023-12-10 12:05:21 +0100

    feat: Added post

--- components/newAuction.tsx
+++ components/newAuction.tsx
@@ -1,55 +1,80 @@
-import React, { useState } from "react";
+"use client";
+
+import { CreateNewAuction } from "@/database/auction_ops";
+import React, { FormEvent, useState } from "react";
 import { Button, Card, Form } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 
 export function NewAuction() {
-
-    const [startDate, setStartDate] = useState(new Date());
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPrice] = useState(0);
     const [endDate, setEndDate] = useState(new Date());
+    //const [picture, setPicture] = useState(new File());
 
-    const handleStartDateChange = (date: React.SetStateAction<Date>) => {
-        setStartDate(date);
+    const handleTitleChange = (event: React.ChangeEvent) => {
+        const title = event.target.value;
+        console.log(event.target);
+        if(title) {
+            console.log(title);
+            setTitle(title);
+        }
     };
 
-    const handleEndDateChange = (date: React.SetStateAction<Date>) => {
-        // Verificar si la fecha de finalización es posterior a la fecha de inicio
-        if (date > startDate) {
-            setEndDate(date);
-        } else {
-            alert("La fecha de finalización debe ser posterior a la fecha de inicio.");
+    const handleDescriptionChange = (event: React.ChangeEvent) => {
+        const description = event.target.value;
+        if(description) {
+            setDescription(description);
         }
+    };
+
+    const handlePriceChange = (event: React.ChangeEvent) => {
+        const price = event.target.value;
+        if(price) {
+            const parsedPrice = parseFloat(price);
+            if(!isNaN(parsedPrice)) {
+                setPrice(parsedPrice);
+            }
+        }
+    };
+
+    const handleEndDateChange = (date: Date | null) => {
+        if(date) {
+            setEndDate(date);
+        }
+    };
+
+    const handleSumbit = (event: FormEvent) => {
+        event.preventDefault();
+
+        CreateNewAuction({
+            Titulo: title,
+            Descripcion: description,
+            "Fecha limite": endDate,
+            "Precio partida": price,
+            Subastador: "653be37c5ee549bea86cd466",
+            Foto: "foto1.png"
+        });
     };
 
     return (
         <Card style={{ marginBottom: "20px" }}>
             <Card.Body>
-                <Form>
+                <Form onSubmit={handleSumbit}>
                     <Form.Group className="mb-3" controlId="formGroupTitle">
                         <Form.Label>¿Qué vas a vender?</Form.Label>
-                        <Form.Control type="title" placeholder="Introduzca titulo" />
+                        <Form.Control onChange={handleTitleChange} type="title" placeholder="Introduzca titulo" />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupDescription">
                         <Form.Label>Detalla el producto</Form.Label>
-                        <Form.Control as="textarea" placeholder="Introduzca descripcion" rows={2} />
+                        <Form.Control onChange={handleDescriptionChange} as="textarea" placeholder="Introduzca descripcion" rows={2} />
                     </Form.Group>
                     <Form.Group controlId="formGroupMinPrice" className="mb-3">
                         <Form.Label>Precio mínimo</Form.Label>
-                        <Form.Control type="number" placeholder="Introduce el precio mínimo" min="1" />
+                        <Form.Control onChange={handlePriceChange} type="number" placeholder="Introduce el precio mínimo" min="1" />
                     </Form.Group>
-                    <Form.Group controlId="formGroupStartDate" className="mb-3">
-                        <Form.Label>Fecha y hora de inicio de la subasta </Form.Label>{" "}
-                        <DatePicker
-                            selected={startDate}
-                            onChange={handleStartDateChange}
-                            showTimeSelect
-                            timeIntervals={15}
-                            timeFormat="HH:mm"
-                            dateFormat="yyyy-MM-dd h:mm aa"
-                        />
-                    </Form.Group>
-
                     <Form.Group controlId="formGroupEndDate" className="mb-3">
                         <Form.Label>Fecha y hora de finalización de la subasta </Form.Label>{" "}
                         <DatePicker
