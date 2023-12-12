65fe196b46a6deb61df4fb9da0b3b1496df9b214
mxke17 <80281934+mxke17@users.noreply.github.com>
2023-12-12 12:46:23 +0100

    Co-authored-by: sergio12mp <sergio12mp@users.noreply.github.com>

--- components/navbarLite.tsx
+++ components/navbarLite.tsx
@@ -3,8 +3,11 @@ import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { signIn, useSession, signOut } from "next-auth/react";
+import Link from "next/link";
 
 export function NavbarLite() {
+
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container fluid>
@@ -12,6 +15,7 @@ export function NavbarLite() {
                 <Nav className="ms-auto" style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
                     <Button variant="outline-primary" className="me-2">Regístrese</Button>
                     <Button href="/login" variant="outline-success">Identifíquese</Button>
+                    <Button onClick={() => signIn()} variant="outline-success">Google</Button>
                 </Nav>
             </Container>
         </Navbar>
