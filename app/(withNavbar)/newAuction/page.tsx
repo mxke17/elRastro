b0f49f2235b93bc13094386a2e50953e7e14ce5f
thicco-catto <cristobal.martin01@gmail.com>
2023-12-28 20:00:45 +0100

    fix: New auction page works properly

--- app/(withNavbar)/newAuction/page.tsx
+++ app/(withNavbar)/newAuction/page.tsx
@@ -1,8 +1,6 @@
-
 import { NewAuction } from "@/components/newAuction";
 import { GetUserByEmail } from "@/database/users";
 import { getServerSession } from "next-auth";
-import { Card, Col, Container, Figure, Row } from "react-bootstrap";
 
 export const dynamic = "force-dynamic";
 
@@ -11,7 +9,6 @@ export default async function newAuction() {
     const session = await getServerSession();
 
     const sessionUser = session?.user;
-
 
     if (!sessionUser) {
         return <></>;
@@ -23,28 +20,11 @@ export default async function newAuction() {
 
     const user = await GetUserByEmail(sessionUser.email);
 
-
-
     if(!user) {
         return <></>;
     }
 
     return <>
-        <Container fluid="md">
-            <Row>
-                <Col xs={1}></Col>
-                <Col>
-                    <Card style={{ marginBottom: "20px" }}>
-                        <Figure.Image
-                            style={{ objectFit: "cover", objectPosition: "center", width: "100%", height: "75px" }}
-                            src="/newAuction.jpg"
-                        />
-                        <Card.Body>¡Tu tesoro merece su momento de gloria! Sube los datos de tu producto y comencemos la puja. ¡La aventura está a punto de empezar!</Card.Body>
-                    </Card>
-                    <NewAuction userID={user.ID.toHexString()}></NewAuction>
-                </Col>
-                <Col xs={1}></Col>
-            </Row>
-        </Container>
+        <NewAuction userID={user.ID.toHexString()}></NewAuction>
     </>;
 }
--- components/newAuction.tsx
+++ components/newAuction.tsx
@@ -2,7 +2,7 @@
 
 import { NewAuctionFromForm } from "@/database/auction_ops";
 import React, { useState } from "react";
-import { Button, Card, Form } from "react-bootstrap";
+import { Button, Card, Col, Container, Figure, Form, Row } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -10,11 +10,30 @@ interface NewAuctionProps {
     userID: string;
 }
 
-export function NewAuction(props : NewAuctionProps) {
+export function NewAuction(props: NewAuctionProps) {
+    return <Container fluid="md">
+        <Row>
+            <Col xs={1}></Col>
+            <Col>
+                <Card style={{ marginBottom: "20px" }}>
+                    <Figure.Image
+                        style={{ objectFit: "cover", objectPosition: "center", width: "100%", height: "75px" }}
+                        src="/newAuction.jpg"
+                    />
+                    <Card.Body>¡Tu tesoro merece su momento de gloria! Sube los datos de tu producto y comencemos la puja. ¡La aventura está a punto de empezar!</Card.Body>
+                </Card>
+                <NewAuctionForm userID={props.userID}></NewAuctionForm>
+            </Col>
+            <Col xs={1}></Col>
+        </Row>
+    </Container>;
+}
+
+export function NewAuctionForm(props: NewAuctionProps) {
     const [endDate, setEndDate] = useState(new Date());
     const userID = props.userID;
     const handleEndDateChange = (date: Date | null) => {
-        if(date) {
+        if (date) {
             setEndDate(date);
         }
     };
@@ -31,15 +50,15 @@ export function NewAuction(props : NewAuctionProps) {
                 <Form action={handleSumbit}>
                     <Form.Group className="mb-3" controlId="formGroupTitle">
                         <Form.Label>¿Qué vas a vender?</Form.Label>
-                        <Form.Control name="title" type="title" placeholder="Introduzca titulo" required/>
+                        <Form.Control name="title" type="title" placeholder="Introduzca titulo" required />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGroupDescription">
                         <Form.Label>Detalla el producto</Form.Label>
-                        <Form.Control name="description" as="textarea" placeholder="Introduzca descripcion" rows={2} required/>
+                        <Form.Control name="description" as="textarea" placeholder="Introduzca descripcion" rows={2} required />
                     </Form.Group>
                     <Form.Group controlId="formGroupMinPrice" className="mb-3">
                         <Form.Label>Precio mínimo</Form.Label>
-                        <Form.Control name="price" type="number" placeholder="Introduce el precio mínimo" min="1" required/>
+                        <Form.Control name="price" type="number" placeholder="Introduce el precio mínimo" min="1" required />
                     </Form.Group>
                     <Form.Group controlId="formGroupEndDate" className="mb-3">
                         <Form.Label>Fecha y hora de finalización de la subasta </Form.Label>{" "}
